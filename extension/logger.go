8b7d40b8e73527d9c79b4339f5d040e771d0fe88
yondercode <yondercode@gmail.com>
2022-05-06 20:11:20 +0700

    Add mutex on logger

--- extension/logger.go
+++ extension/logger.go
@@ -1,14 +1,28 @@
 package extension
 
 import (
+	"sync"
+	"time"
+
 	"github.com/flowscan/lapis"
 	"github.com/rs/zerolog/log"
 )
+
+var layerStartAt map[string]map[uint64]time.Time
+var mu sync.RWMutex
 
 // Logger is an extension for lapis that logs access and value sets for debugging
 type Logger[TKey comparable, TValue any] struct{}
 
 func (e Logger[TKey, TValue]) Name() string { return "Logger" }
+
+func (e Logger[TKey, TValue]) InitializationHook(layers []lapis.Layer[TKey, TValue]) error {
+	layerStartAt = make(map[string]map[uint64]time.Time)
+	for _, layer := range layers {
+		layerStartAt[layer.Identifier()] = make(map[uint64]time.Time)
+	}
+	return nil
+}
 
 func (e Logger[TKey, TValue]) PreLoadHook(traceID uint64, keys []TKey) []error {
 	log.Debug().Uint64("trace", traceID).Msgf("start loading: %v", keys)
@@ -21,11 +35,23 @@ func (e Logger[TKey, TValue]) PostLoadHook(traceID uint64, keys []TKey, values [
 }
 
 func (e Logger[TKey, TValue]) LayerPreLoadHook(traceID uint64, layer lapis.Layer[TKey, TValue], keys []TKey) []error {
-	log.Debug().Uint64("trace", traceID).Msgf("start loading at layer %v: %v", layer.Identifier(), keys)
+	mu.Lock()
+	layerStartAt[layer.Identifier()][traceID] = time.Now()
+	mu.Unlock()
+	log.Debug().Uint64("trace", traceID).Msgf("start loading at layer %v", layer.Identifier())
 	return nil
 }
 
 func (e Logger[TKey, TValue]) LayerPostLoadHook(traceID uint64, layer lapis.Layer[TKey, TValue], keys []TKey, values []TValue, errors []error) []error {
-	log.Debug().Uint64("trace", traceID).Msgf("finish loading from layer %v: %v (errors: %v)", layer.Identifier(), values, errors)
+	mu.Lock()
+	log.Debug().Uint64("trace", traceID).Msgf(
+		"finish loading from layer %v: %v (errors: %v) time: %v",
+		layer.Identifier(),
+		values,
+		errors,
+		time.Since(layerStartAt[layer.Identifier()][traceID]).Milliseconds(),
+	)
+	delete(layerStartAt[layer.Identifier()], traceID)
+	mu.Unlock()
 	return nil
 }
