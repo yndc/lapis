7edce5e2b17e637014a6035d4faaeaa39443631e
yondercode <yondercode@gmail.com>
2023-03-02 19:14:29 +0700

    Fix logger on conflicting map

--- extension/logger.go
+++ extension/logger.go
@@ -9,27 +9,26 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-var loggerLayerLoadStartAt map[string]map[uint64]time.Time
-var loggerLayerSetStartAt map[string]map[uint64]time.Time
-var loggerMu sync.RWMutex
-
 // Logger is an extension for lapis that logs access and value sets for debugging
 type Logger[TKey comparable, TValue any] struct {
-	layers []lapis.Layer[TKey, TValue]
-	logger zerolog.Logger
+	layers                 []lapis.Layer[TKey, TValue]
+	logger                 zerolog.Logger
+	loggerMu               sync.RWMutex
+	loggerLayerLoadStartAt []map[uint64]time.Time
+	loggerLayerSetStartAt  []map[uint64]time.Time
 }
 
 func (e *Logger[TKey, TValue]) Name() string { return "Logger" }
 
 func (e *Logger[TKey, TValue]) InitializationHook(r *lapis.Store[TKey, TValue], layers []lapis.Layer[TKey, TValue]) error {
 	e.layers = layers
-	loggerLayerLoadStartAt = make(map[string]map[uint64]time.Time)
-	loggerLayerSetStartAt = make(map[string]map[uint64]time.Time)
+	e.loggerLayerLoadStartAt = make([]map[uint64]time.Time, len(layers))
+	e.loggerLayerSetStartAt = make([]map[uint64]time.Time, len(layers))
 	e.logger = log.With().Str("store", r.Identifier()).Logger()
 	e.logger.Debug().Msgf("store initialized")
-	for _, layer := range layers {
-		loggerLayerLoadStartAt[layer.Identifier()] = make(map[uint64]time.Time)
-		loggerLayerSetStartAt[layer.Identifier()] = make(map[uint64]time.Time)
+	for layerIndex := range layers {
+		e.loggerLayerLoadStartAt[layerIndex] = make(map[uint64]time.Time)
+		e.loggerLayerSetStartAt[layerIndex] = make(map[uint64]time.Time)
 	}
 	return nil
 }
@@ -45,24 +44,24 @@ func (e *Logger[TKey, TValue]) PostLoadHook(traceID uint64, keys []TKey, values 
 }
 
 func (e *Logger[TKey, TValue]) LayerPreLoadHook(traceID uint64, layerIndex int, keys []TKey) []error {
-	loggerMu.Lock()
-	loggerLayerLoadStartAt[e.layers[layerIndex].Identifier()][traceID] = time.Now()
-	loggerMu.Unlock()
+	e.loggerMu.Lock()
+	e.loggerLayerLoadStartAt[layerIndex][traceID] = time.Now()
+	e.loggerMu.Unlock()
 	e.logger.Debug().Uint64("trace", traceID).Msgf("loading start at layer %v: %v", e.layers[layerIndex].Identifier(), keys)
 	return nil
 }
 
 func (e *Logger[TKey, TValue]) LayerPostLoadHook(traceID uint64, layerIndex int, keys []TKey, values []TValue, errors []error) []error {
-	loggerMu.Lock()
+	e.loggerMu.Lock()
 	e.logger.Debug().Uint64("trace", traceID).Msgf(
 		"loading finish from layer %v: %v (errors: %v) time: %v",
 		e.layers[layerIndex].Identifier(),
 		values,
 		errors,
-		time.Since(loggerLayerLoadStartAt[e.layers[layerIndex].Identifier()][traceID]).Milliseconds(),
+		time.Since(e.loggerLayerLoadStartAt[layerIndex][traceID]).Milliseconds(),
 	)
-	delete(loggerLayerLoadStartAt[e.layers[layerIndex].Identifier()], traceID)
-	loggerMu.Unlock()
+	delete(e.loggerLayerLoadStartAt[layerIndex], traceID)
+	e.loggerMu.Unlock()
 
 	// // log the status for each result
 	// var status string
@@ -81,23 +80,23 @@ func (e *Logger[TKey, TValue]) LayerPostLoadHook(traceID uint64, layerIndex int,
 }
 
 func (e *Logger[TKey, TValue]) LayerPreSetHook(traceID uint64, layerIndex int, keys []TKey, values []TValue) []error {
-	loggerMu.Lock()
-	loggerLayerSetStartAt[e.layers[layerIndex].Identifier()][traceID] = time.Now()
-	loggerMu.Unlock()
+	e.loggerMu.Lock()
+	e.loggerLayerSetStartAt[layerIndex][traceID] = time.Now()
+	e.loggerMu.Unlock()
 	e.logger.Debug().Uint64("trace", traceID).Msgf("setting start at layer %v: keys: %v values: %v", e.layers[layerIndex].Identifier(), keys, values)
 	return nil
 }
 
 func (e *Logger[TKey, TValue]) LayerPostSetHook(traceID uint64, layerIndex int, keys []TKey, values []TValue, errors []error) {
-	loggerMu.Lock()
+	e.loggerMu.Lock()
 	e.logger.Debug().Uint64("trace", traceID).Msgf(
 		"setting finish at layer %v: keys: %v values: %v errors: %v time: %v",
 		e.layers[layerIndex].Identifier(),
 		keys,
 		values,
 		errors,
-		time.Since(loggerLayerSetStartAt[e.layers[layerIndex].Identifier()][traceID]).Milliseconds(),
+		time.Since(e.loggerLayerSetStartAt[layerIndex][traceID]).Milliseconds(),
 	)
-	delete(loggerLayerSetStartAt[e.layers[layerIndex].Identifier()], traceID)
-	loggerMu.Unlock()
+	delete(e.loggerLayerSetStartAt[layerIndex], traceID)
+	e.loggerMu.Unlock()
 }
